ae1570cc716cd17c3205609e39fb9f9bf33d4310
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-13 19:37:23 -0800

    Added Tags CRUD to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :drills
+  resources :tags
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
