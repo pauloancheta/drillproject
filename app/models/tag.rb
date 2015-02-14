90b194f06aba6e4f61bbc81bb2aa527abf12aef0
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-13 19:31:06 -0800

    Added Tag model validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,15 @@
 class Tag < ActiveRecord::Base
+
+  # Ensures the tag's title exists, and is unique
+  validates :title, presence: true, uniqueness: true
+  # Ensures the tag's title is all lowercase and 
+  # spaces are replaced with underscores
+  before_validation :tag_styling
+
+  private
+
+  def tag_styling
+    self.title = title.downcase.gsub(/\s/, '_')
+  end
+
 end
