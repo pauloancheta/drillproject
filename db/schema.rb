46a1024b4f2b771b8f8b7e2c826b2c116998e708
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-13 18:39:27 -0800

    Added the Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 20150214030911) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
