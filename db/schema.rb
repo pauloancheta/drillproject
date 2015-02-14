4ce51e1cc2522b328c05a07bae804f0816a45f27
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-13 18:39:27 -0800

    Added the Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -27,4 +27,10 @@ ActiveRecord::Schema.define(version: 20150214025120) do
   add_index "solutions", ["admin_id"], name: "index_solutions_on_admin_id", using: :btree
   add_index "solutions", ["drill_id"], name: "index_solutions_on_drill_id", using: :btree
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
