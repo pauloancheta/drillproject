c7ed87d388d292f4a5521f426ff3a5dbd954c698
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-14 20:49:54 -0800

    Addressed user references in subscriptions controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
   def create
     @drill_group = DrillGroup.find params[:drill_group_id]
     @subscription = @drill_group.subscriptions.new
-    @subscription.user = User.last
+    @subscription.user = current_user
     if @subscription.save
       redirect_to @drill_group
     else
