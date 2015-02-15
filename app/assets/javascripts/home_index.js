a061181cfd271a271bcc51eded5cc1cd365fb34f
Cooper Dean <cooperdean30@gmail.com>
2015-02-15 12:17:11 -0800

    navbar overhaul, homepage styling

--- app/assets/javascripts/home_index.js
+++ app/assets/javascripts/home_index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
 
+$('.navlink').css('text-decoration', 'none')
+
 // POSTER BUTTON ANIMATION
 $('.poster').hover(function () {
   $(this).children('.button').animate({'bottom': '-=20px'}, 1);
@@ -8,6 +10,21 @@ $('.poster').hover(function () {
   $(this).children('.button').css('bottom', '0px');
 });
 
+// NAVBUTTON
+$('.navbutton').hover(function () {
+  $(this).css('background', '#4d496c');
+  $(this).find('.navlink').css('color', 'white');
+},function () {
+  $(this).css('background', 'white');
+  $(this).find('.navlink').css('color', '#4d496c');
+});
+
+// NAVLINK
+$('.navlink').hover(function () {
+  $(this).css('color', '#40daf4');
+},function () {
+  $(this).css('color', 'white');
+});
 
 
 })
