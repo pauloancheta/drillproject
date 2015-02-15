1e073b360aae36d64070c8c3f91d29169544318a
Derek Barker <derekbarker95@gmail.com>
2015-02-14 20:05:39 -0800

    about page styled

--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,6 @@ ActiveRecord::Schema.define(version: 20150215002553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.string   "password_confirmation"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
   create_table "drill_groups", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
