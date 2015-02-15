4d5930addfd2e5f385cb5c3e601f55bb62f3c11c
Ben Vause <benjvause@gmail.com>
2015-02-14 16:42:14 -0800

    working on subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,26 @@
 class SubscriptionsController < ApplicationController
+
+  def create
+    drill_group = DrillGroup.find params[:drill_group_id]
+    subscription = drill_group.subscriptions.new
+    if subscription.save
+      redirect_to drill_group, notice: "Subscribed!"
+    else
+      redirect_to drill_group, alert: "Subscribed Failed"
+    end
+  end
+
+
+  def destroy
+    drill_group = DrillGroup.find params[:drill_group_id]
+    subscription = drill_group.subscriptions.find params[:id]
+    if subscription.destroy
+      redirect_to drill_group, notice: "Unsubscribed!"
+    else
+      redirect_to drill_group, alert: "Cannot unsubscribe!"
+    end
+        
+  end
+
+
 end
