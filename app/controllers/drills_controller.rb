83e1055ac0696579db660f0fb75e490f0b034cfc
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-15 23:52:01 -0800

    Got 3 main Tag buttons to link to the associated pages

--- app/controllers/drills_controller.rb
+++ app/controllers/drills_controller.rb
@@ -4,7 +4,7 @@ class DrillsController < ApplicationController
 
   # Get the desired drill for the show, edit,
   # update, and destroy actions before they execute
-  before_action :get_drill, only: [:show, :edit, :update, :destroy]
+  before_action :get_drill, only: [:show, :edit, :update, :destroy, :attempt]
   # respond_to :js
 
   def index
@@ -63,6 +63,10 @@ class DrillsController < ApplicationController
       flash[:alert] = get_errors
       redirect_to [@drill_group, @drill]
     end
+  end
+
+  def attempt
+
   end
 
   private
