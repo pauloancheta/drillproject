dcd8115c8d7283cb80bddc2e89827c5aa601684a
Derek Barker <derekbarker95@gmail.com>
2015-02-15 20:05:40 -0800

    about page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-var WelcomeMsg = ["Hey", "Welcome", "Hello", "Whats Up", "Good day", "Whats Kicking"];
+var WelcomeMsg = ["Hey", "Welcome", "Hello", "Whats Up", "Good day", "Whats Kicking", "Ahoy", "G'day", "Salutations", "Hej", "Bonjour", "Guten Tag", "Aloha", "¡Hola!"];
   
   var msgselect = Math.floor((Math.random() * WelcomeMsg.length));
  $('#greeting').prepend(WelcomeMsg[msgselect] + "  ");
+
+
+ var MotivationMsg = [
+                      {txt: "You Can Do It!", person: "Rob Schneider"},
+                      {txt: "You Can Do It!", person: "Rob Schneider"},
+                      {txt: "You Can Do It!", person: "Rob Schneider"},
+                      {txt: "You Can Do It!", person: "Rob Schneider"},
+                      {txt: "You Can Do It!", person: "Rob Schneider"},
+                      {txt: "You Can Do It!", person: "Rob Schneider"},
+                      ]
 
 })
 
