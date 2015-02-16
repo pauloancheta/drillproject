dae25e2b6777971045b01b4bea71a1ad61e159c0
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-02-15 22:39:49 -0800

    added javascript to answer questions

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
   $('#attempt_submit').on('click', function() {
-    if($('#attempt_text').val() == $('#0').text()) {
-      alert('lol');
+    var i = 0;
+    for(i = 0; i < 100; i++){
+      if($('#attempt_text').val() == $('#' + i).text()) {
+        $('.correctdiv').fadeIn();
+        $('.incorrectdiv').hide();
+        break;
+      };
+      if(i == 99){
+        $('.incorrectdiv').fadeIn();
+        $('.correctdiv').fadeOut();
+      };
     };
   });
 });
