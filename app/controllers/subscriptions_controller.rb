6688d35d3617ffe230775f0b469da3938d0020bf
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-23 14:49:20 -0800

    Added AJAX to the MyDrills page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -14,12 +14,15 @@ class SubscriptionsController < ApplicationController
   def destroy
     @drill_group = DrillGroup.find params[:drill_group_id]
     @subscription = @drill_group.subscriptions.find params[:id]
-    if @subscription.destroy
-      redirect_to all_drills_path
-    else
-      redirect_to my_drills_path, alert: get_errors
+    respond_to do |format|
+      if @subscription.destroy
+        format.html { redirect_to my_drills_path }
+        format.js { render }
+      else
+        format.html { redirect_to my_drills_path, alert: get_errors }
+        format.js { render }
+      end
     end
-        
   end
 
   private
