b1c857a6d861f10d98d43f5070cb6b7da6284a1e
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-23 14:35:26 -0800

    Fixed bug with disappearing links

--- app/assets/javascripts/home_index.js
+++ app/assets/javascripts/home_index.js
@@ -1,37 +1,32 @@
 $(document).ready(function () {
 
+  $('.navlink').css('text-decoration', 'none');
 
-$('.navlink').css('text-decoration', 'none')
-$('a').css('text-decoration', 'none')
-// POSTER BUTTON ANIMATION
-$('.poster').hover(function () {
-  $(this).children('.button').animate({'bottom': '-=20px'}, 1);
-},function () {
-  $(this).children('.button').css('bottom', '0px');
+  $('a').css('text-decoration', 'none');
+
+  // POSTER BUTTON ANIMATION
+  $('.poster').hover(function() {
+    $(this).children('.button').animate({'bottom': '-=20px'}, 1);
+  }, function() {
+    $(this).children('.button').css('bottom', '0px');
+  });
+
+  // NAVBUTTON
+  $('.navbutton').hover(function() {
+    $(this).css('background', '#4d496c');
+    $(this).find('.navlink').css('color', 'white');
+    $(this).find('a').css('color', 'white');
+  }, function() {
+    $(this).css('background', 'white');
+    $(this).find('.navlink').css('color', '#4d496c');
+    $(this).find('a').css('color', '#4d496c');
+  });
+
+  // NAVLINK
+  $('.navlink').hover(function() {
+    $(this).css('color', '#40daf4');
+  },function() {
+    $(this).css('color', 'white');
+  });
+
 });
-
-// NAVBUTTON
-$('.navbutton').hover(function () {
-  $(this).css('background', '#4d496c');
-  $(this).find('.navlink').css('color', 'white');
-  $(this).find('a').css('color', 'white');
-},function () {
-  $(this).css('background', 'white');
-  $(this).find('.navlink').css('color', '#4d496c');
-  $(this).find('a').css('color', '#4d496c');
-});
-
-// NAVLINK
-$('.navlink').hover(function () {
-  $(this).css('color', '#40daf4');
-},function () {
-  $(this).css('color', 'white');
-});
-
-$('a').hover(function () {
-  $(this).css('color', '#40daf4');
-},function () {
-  $(this).css('color', 'white');
-}) 
-
-})
