6e17bf4dc1c6df3f604651537f9dadc1ee64ea08
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-23 19:33:59 -0800

    Added AJAX to delete DrillGroups

--- app/controllers/drill_groups_controller.rb
+++ app/controllers/drill_groups_controller.rb
@@ -51,11 +51,14 @@ class DrillGroupsController < ApplicationController
 
   def destroy
     @drill_group = DrillGroup.find(params[:id])
-    if @drill_group.destroy
-      redirect_to drill_groups_path 
-    else
-      flash[:alert] = get_errors
-      redirect_to @drill_group
+    respond_to do |format|
+      if @drill_group.destroy
+        format.html { redirect_to drill_groups_path }
+        format.js { render } 
+      else
+        format.html { redirect_to @drill_group, alert: get_errors }
+        format.js { render }
+      end
     end
   end
 
@@ -64,7 +67,7 @@ class DrillGroupsController < ApplicationController
   private
 
   def drill_group_params
-    params.require(:drill_group).permit(:name, :description, :level_id)
+    params.require(:drill_group).permit(:name, :description, :level_id, :tag_ids)
   end
 
   def get_errors
