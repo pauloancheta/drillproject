207057bec70fbd95f6e3cc8e60e572712d0b834d
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-24 19:32:44 -0800

    Made user logins and registrations occur via AJAX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,20 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path, notice: "Account Created!"
-    else
-      flash[:alert] = "Account not created"
-      render :new
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: "Account Created!" }
+        format.js { render js: "window.location = '/'" }
+      else
+        format.html {
+          flash[:alert] = "Account not created"
+          render :new
+        }
+        format.js {
+          render
+        }
+      end
     end
   end
   
